b6977057e4bb656298601abf5d3c92ffd60bb4e1
taetoo <rlaougs5@naver.com>
2023-02-22 12:01:32 +0900

    Add: UserRequestDto 인자 추가

--- src/main/java/com/sideproject/fikabackend/user/dto/UserRequestDto.java
+++ src/main/java/com/sideproject/fikabackend/user/dto/UserRequestDto.java
@@ -1,4 +1,19 @@
 package com.sideproject.fikabackend.user.dto;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+
+@NoArgsConstructor
+@Getter
 public class UserRequestDto {
+
+    private String uId;
+    private String pw;
+    private Long userName;
+    private String nickName;
+    private String region;
+    private int age;
 }
